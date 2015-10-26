dc32f3c5d25ac2be721e90e4ec7aad5abc495e46
Filip Procházka <filip@prochazka.su>
2015-10-26 18:09:31 +0100

    PrivateKey & PublicKey

---
+++ src/Certificate/PrivateKey.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\CsobClient\Certificate;
+
+use Kdyby\CsobClient\IOException;
+use Kdyby\CsobClient\MissingExtensionException;
+use Kdyby\CsobClient\SigningException;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ * @see https://iplatebnibrana.csob.cz/keygen/
+ */
+class PrivateKey
+{
+
+	/**
+	 * @var string
+	 */
+	private $privatePath;
+
+	/**
+	 * @var string
+	 */
+	private $password;
+
+
+
+	public function __construct($privateKeyPath, $password)
+	{
+		$this->privatePath = $privateKeyPath;
+		$this->password = $password;
+	}
+
+
+
+	public function sign($text)
+	{
+		if (!function_exists('openssl_get_privatekey')) {
+			throw new MissingExtensionException('OpenSSL extension in PHP is required. Please install or enable it.');
+		}
+
+		$privateKey = @file_get_contents($this->privatePath);
+		if ($privateKey === FALSE) {
+			throw new IOException(sprintf('The private key was not found at %s', $this->privatePath));
+		}
+
+		$privateKeyId = openssl_get_privatekey($privateKey, $this->password);
+		if ($privateKeyId === FALSE) {
+			throw new SigningException(sprintf('Private key %s couldn\'t be opened.', $this->privatePath));
+		}
+
+		$res = openssl_sign($text, $signature, $privateKeyId);
+		$signature = base64_encode($signature);
+		openssl_free_key($privateKeyId);
+
+		if ($res !== TRUE) {
+			throw new SigningException('Signing failed');
+		}
+
+		return $signature;
+	}
+
+}
---
+++ src/Certificate/PublicKey.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\CsobClient\Certificate;
+
+use Kdyby\CsobClient\IOException;
+use Kdyby\CsobClient\MissingExtensionException;
+use Kdyby\CsobClient\SigningException;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ * @see https://iplatebnibrana.csob.cz/keygen/
+ */
+class PublicKey
+{
+
+	/**
+	 * @var string
+	 */
+	private $publicPath;
+
+
+
+	public function __construct($bankPublicKeyPath)
+	{
+		$this->publicPath = $bankPublicKeyPath;
+	}
+
+
+
+	public function verify($text, $signatureBase64)
+	{
+		if (!function_exists('openssl_get_privatekey')) {
+			throw new MissingExtensionException('OpenSSL extension in PHP is required. Please install or enable it.');
+		}
+
+		$publicKey = @file_get_contents($this->publicPath);
+		if ($publicKey === FALSE) {
+			throw new IOException(sprintf('The public key was not found at %s', $this->publicPath));
+		}
+
+		$publicKeyId = openssl_get_publickey($publicKey);
+		if ($publicKeyId === FALSE) {
+			throw new SigningException(sprintf('Public key %s couldn\'t be opened.', $this->publicPath));
+		}
+
+		$signature = base64_decode($signatureBase64);
+		$res = openssl_verify($text, $signature, $publicKeyId);
+		openssl_free_key($publicKeyId);
+
+		if ($res == -1) {
+			throw new SigningException(sprintf('Verification of signature failed: %s', openssl_error_string()));
+		}
+
+		return $res == 1;
+	}
+
+}
